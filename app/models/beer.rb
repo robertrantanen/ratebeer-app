748a1d777f5672f379e0ff7e8110017d51476095
Robert <robert.j.rantanen@helsinki.fi>
2022-09-11 20:43:03 +0300

    week2 start

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
+    has_many :ratings
+
+    def average_rating 
+        return ratings.inject(0.0){ |sum, r| sum + r.score } / ratings.size
+        #sum=0
+        #ratings.each do |rating|
+        #    sum = sum + rating.score.to_f
+        #end
+        #return sum / ratings.size
+    end
 end
