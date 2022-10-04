acb0b44fc56266d4af0a26556e10c2ed57024676
Robert <robert.j.rantanen@helsinki.fi>
2022-10-04 11:29:39 +0300

    week5 2,3

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+  it "if multiple are returned by the API, they are shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ), Place.new( name: "Gurula", id: 2 ), Place.new( name: "Unicafe", id: 3 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Gurula"
+    expect(page).to have_content "Unicafe"
+  end
+  it "if none are returned by the API, the page shows no locations found" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ), Place.new( name: "Gurula", id: 2 ), Place.new( name: "Unicafe", id: 3 ) ]
+    )
+    allow(BeermappingApi).to receive(:places_in).with("kampala").and_return(
+      []
+    )
+
+    visit places_path
+    fill_in('city', with: 'kampala')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kampala"
+  end
+end
