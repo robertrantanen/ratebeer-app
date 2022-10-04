3824505f13b68173b87f886da1768f22dbffacdc
Robert <robert.j.rantanen@helsinki.fi>
2022-10-04 14:48:35 +0300

    week5 continues

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,19 +1,24 @@
 class BeermappingApi
-    def self.places_in(city)
-      url = "http://beermapping.com/webservice/loccity/#{key}/"
-  
-      response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-      places = response.parsed_response["bmp_locations"]["location"]
-  
-      return [] if places.is_a?(Hash) and places['id'].nil?
-  
-      places = [places] if places.is_a?(Hash)
-      places.map do | place |
-        Place.new(place)
-      end
+  def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) && places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do |place|
+      Place.new(place)
     end
-  
-    def self.key
-      "5d44d431eaec0b847c5067ead93b0de3"
-    end
+  end
+
+  def self.key
+    "5d44d431eaec0b847c5067ead93b0de3"
+  end
 end
