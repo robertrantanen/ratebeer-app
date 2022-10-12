e3ad74d2f3bb1755f72192cbe1533d8873639eee
Robert <robert.j.rantanen@helsinki.fi>
2022-10-12 10:29:53 +0300

    fix some stuff

--- lib/weather_api.rb
+++ lib/weather_api.rb
@@ -1,15 +1,10 @@
 class WeatherApi
-  def self.weather_in(city)
-    city = city.downcase
-    Rails.cache.fetch(city, expires_in: 1.days) { get_weather_in(city) }
-  end
-
   def self.get_weather_in(city)
     url = "http://api.weatherstack.com/current?access_key=#{key}&query="
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
 
-    return response.parsed_response["current"]
+    response.parsed_response["current"]
   end
 
   def self.key
